ec6f64c76914d5dcb42fd9bf514fdfd009f15ab1
meghanaSattarapu <163449773+meghanaSattarapu@users.noreply.github.com>
2025-11-16 23:33:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def wordle():
+    return render_template("wordle.html")
 
-@app.route('/hotel1')
-def hotel1():
-    return render_template('hotel1.html')
-
-@app.route('/hotel2')
-def hotel2():
-    return render_template('hotel2.html')
-
-@app.route('/hotel3')
-def hotel3():
-    return render_template('hotel3.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
